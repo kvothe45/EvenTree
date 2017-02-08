482b7cfdb3be857790af6fbe9f67826d1dcd2ef7
Ralph E. Beard IV <ralph.e.beard.iv@outlook.com>
2017-02-07 20:44:10 -0500

    uploaded project

---
+++ EvenTree/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EvenTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("How large do you want your tree base to be?");
+            int initialNumber = int.Parse(Console.ReadLine());
+            int baseLength = initialNumber;
+
+            for (int i = 1; i <= initialNumber; i++)
+            {
+                for (int k = 1; k <= baseLength - 1; k++)
+                    Console.Write(" ");
+                baseLength--;
+                for (int j = i; j >= 1; j--)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine("");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
